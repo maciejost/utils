74094bb78ed04a2c63b4f2a8bc6b5ddd0326adcf
Maciej Ostrowski <maciej.ostrowski@fremtind.no>
2024-09-07 20:27:52 +0200

    Stil dialog og container

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type ContainerProps = {
   children: React.ReactNode;
@@ -15,25 +15,59 @@ export const Container: React.FC<ContainerProps> = ({
 }) => {
   const [isAppletOpen, setIsAppletOpen] = useState(false);
 
-  return (
-    <button
-      className="p-16 md:px-24 md:py-32 bg-background-page shadow-md hover:bg-background-container-high transition-all duration-300 ease-in-out rounded-lg hover:-translate-y-8 "
-      onClick={() => setIsAppletOpen(!isAppletOpen)}
-    >
-      <div className="heading-2" aria-hidden="true">
-        {appletIcon}
-      </div>
-      <div>
-        <h2 className="heading-2 mb-12">{appletTitle}</h2>
-        <p className="text-left">{appletDescription}</p>
-      </div>
-      <dialog open={isAppletOpen}>
-        <button onClick={() => setIsAppletOpen(false)}>
+  useEffect(() => {
+    if (isAppletOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [isAppletOpen]);
+
+  const AppletDialog = (
+    <>
+      {isAppletOpen && (
+        <button
+          onClick={() => setIsAppletOpen(false)}
+          aria-hidden="true"
+          tabIndex={-1}
+          className="w-screen h-screen absolute backdrop-blur-sm bg-granitt bg-opacity-85 inset-0 -z-1"
+        />
+      )}
+      <dialog
+        className="absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 z-50 w-full max-w-screen-sm min-w-[380px] p-16 md:p-40 "
+        open={isAppletOpen}
+      >
+        <button
+          className="absolute top-8 right-16"
+          onClick={() => setIsAppletOpen(false)}
+        >
           <span aria-hidden="true">❌</span>
           <span className="sr-only">Close {appletTitle}-applet</span>
         </button>
+        <div className="mb-24 md:mb-40">
+          <h2 className="heading-2 mb-12">{appletTitle}</h2>
+          <p className="text-left max-w-[468px]">{appletDescription}</p>
+        </div>
         {children}
       </dialog>
-    </button>
+    </>
+  );
+
+  return (
+    <>
+      <button
+        className="p-16 md:px-24 md:py-32 bg-background-page shadow-md hover:bg-background-container-high transition-all duration-150 ease-in-out rounded-lg hover:-translate-y-8 "
+        onClick={() => setIsAppletOpen(!isAppletOpen)}
+      >
+        <div className="heading-2" aria-hidden="true">
+          {appletIcon}
+        </div>
+        <div>
+          <h2 className="heading-2 mb-12">{appletTitle}</h2>
+          <p className="text-left">{appletDescription}</p>
+        </div>
+      </button>
+      {AppletDialog}
+    </>
   );
 };
